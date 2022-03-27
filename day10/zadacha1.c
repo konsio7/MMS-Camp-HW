346f4daf2adb73b358b7f3810cda35054e18a11b
konsio7 <kosio353@gmail.com>
2022-03-27 21:39:23 +0300

    HW

--- day10/zadacha1.c
+++ day10/zadacha1.c
@@ -7,9 +7,19 @@ typedef union {
 	uint32_t byte;
 } ufloat;
 
+void printFloatBits(float f){
+	ufloat fl1;
+	fl1.f = f;
+	uint32_t mask = 1 << 31;
+	do{
+		if(mask == 0x40000000 || mask == 0x400000) putchar (' ');
+		putchar(fl1.byte & mask ? '1' : '0');
+	} while (mask >>=1);
+}
+
 int main(){
-	ufloat input;
-	scanf("%lf", &input.f);
-	printf("%lu\n", input.byte);
+	float input = 3.1415926f;
+	printFloatBits(input);
+	printf("\n");
 	return 0;
 }
